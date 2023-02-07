04d2e2b8d02ddbee34622abe8543ccf265ea3450
Navodayan Amit <98446912+a21amit@users.noreply.github.com>
2023-02-07 22:04:09 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,9 @@
+let array=[1,2,3,4,5,6,7]
+let splarr=array.splice(2,4,8,)
+console.log(splarr)
+console.log(array)
+let cp=require("child_process")
+//cp.execSync("code")
+cp.execSync("start chrome")
+console.log(help)
+
