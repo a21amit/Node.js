6e472d13d092119acd1ea153b6ec9185355caffa
Navodayan Amit <98446912+a21amit@users.noreply.github.com>
2023-02-07 22:07:34 +0530

    Update array.js

--- array.js
+++ array.js
@@ -1,9 +1,8 @@
+//array defining 
 let array=[1,2,3,4,5,6,7]
-let splarr=array.splice(2,4,8,)
-console.log(splarr)
+
+//Slicing from index no 
+let splar=array.splice(2,4,8)
+console.log(splar)
 console.log(array)
-let cp=require("child_process")
-//cp.execSync("code")
-cp.execSync("start chrome")
-console.log(help)
 
