96aa38d0d459b55092099188a0dcd786b5d73169
Navodayan Amit <98446912+a21amit@users.noreply.github.com>
2023-02-07 22:11:52 +0530

    Create Childprocess.js

---
+++ Childprocess.js
@@ -0,0 +1,14 @@
+//importing 
+let cp=require("child_process"):
+
+//Open vs code
+cp.execSync("code");
+
+//calculator
+cp.execSync("calc");
+
+//starting browser
+cp.execSync("start chrome https:\\www.codemylife.org")
+
+//for help about module
+console.log(help)
